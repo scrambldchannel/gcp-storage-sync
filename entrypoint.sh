ba37469ef1b8ad28b057b310cb0e94b04918ae77
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2021-12-23 13:51:18 +0100

    feat: remove recursion by default

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ fi
 echo $GCP_SERVICE_ACCOUNT_KEY_FILE > json_file.json
 gcloud auth activate-service-account --key-file json_file.json
 
-sh -c "gsutil -m rsync $* -r ${SOURCE_DIR} gs://${GCP_STORAGE_BUCKET}/${DEST_DIR}"
+sh -c "gsutil -m rsync $* ${SOURCE_DIR} gs://${GCP_STORAGE_BUCKET}/${DEST_DIR}"
 
 gcloud auth revoke --all
 shred -zvu -n 5 json_file.json
